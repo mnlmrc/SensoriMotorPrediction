de36822b8e1c41cec0785067ef325d29a3cf4493
Marco Emanuele <97095997+mnlmrc@users.noreply.github.com>
2025-01-29 14:34:57 -0500

    Update make_TGT.py

--- smp2/make_TGT.py
+++ smp2/make_TGT.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 experiment = 'smp2'
-subj = '104'
-session = 'scanning'  # training or scanning
+subj = '106'
+session = 'training'  # training or scanning
 nblocks = 10
 
 for block in range(nblocks):
