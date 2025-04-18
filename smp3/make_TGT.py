a9fce2f39dd6102af59cfd67fc0d8133eeba7723
Marco Emanuele <97095997+mnlmrc@users.noreply.github.com>
2025-04-18 16:30:08 -0400

    Update make_TGT.py

--- smp3/make_TGT.py
+++ smp3/make_TGT.py
@@ -1,42 +1,35 @@
 import pandas as pd
+import numpy as np
 
 experiment = 'smp3'
-subj = '111'
-session = 'scanning'  # training or scanning
-nblocks = 10
+sn = 100
+nruns = 10
 
-for block in range(nblocks):
-    tgt = pd.read_csv(f'target/{experiment}_template_{session}.tgt', sep="\t")  # read template file
+for run in range(nruns):
 
-    go_rows = tgt['GoNogo'] == 'go'
-    shuffled = tgt.loc[go_rows, ['planTime', 'iti']].sample(frac=1)
-    planTime = shuffled['planTime'].reset_index(drop=True).to_list()
-    iti = shuffled['iti'].reset_index(drop=True).to_list()
-    # planTime_id_shuffled = planTime_shuffled.index.tolist()
-    # planTime_shuffled = planTime_shuffled.reset_index(drop=True).to_list()
-    tgt.loc[go_rows, 'planTime'] = planTime
-    tgt.loc[go_rows, 'iti'] = iti
-    # tgt.loc[go_rows, 'iti'] = tgt.loc[planTime_id_shuffled, 'iti']
+    # Load the file
+    df = pd.read_csv("target/smp3_template.tgt", sep="\t")
 
-    et = tgt.endTime  # extrapolate endTime
-    st = tgt.startTimeDiff  # extrapolate startTimeDiff
-    st1 = st.iloc[[0]]  # extrapolate first startTime
-    st_rest = st.iloc[1:]  # extrapolate rest of startTimeDiff
-    st_rest = st_rest.sample(frac=1).reset_index(drop=True)  # shuffle intervals
-    st = pd.concat([st1, st_rest]).reset_index(drop=True)  # re-attach first sample
+    # Shuffle planTime independently
+    df['planTime'] = np.random.permutation(df['planTime'].values)
 
-    # create startTime with shuffled intervals
-    for s, d in enumerate(st.iloc[1:]):
-        st.iloc[[s+1]] = d + st.iloc[[s]]
+    # Shuffle iti, TrigBaseline, stimTrigBaseline together
+    shuffled_group1 = df[['iti', 'TrigBaseline', 'stimTrigBaseline']].sample(frac=1).reset_index(drop=True)
+    df[['iti', 'TrigBaseline', 'stimTrigBaseline']] = shuffled_group1
 
-    tgt = tgt.sample(frac=1).reset_index(drop=True)  # shuffle template (without startTime)
-    tgt = tgt.drop('startTimeDiff', axis=1)  # drop startTimeDiff column
-    tgt['startTime'] = st  # add startTime to target
-    tgt['endTime'] = et  # add endTime to target
-    tgt['subNum'] = subj
+    # Shuffle the rest of the fields together
+    shuffled_group2 = df[['subNum', 'cueID', 'stimFinger', 'execMaxTime', 'feedbackTime', 'trialLabel', 'TrigExec',
+                          'TrigPlan', 'stimTrigExec', 'stimTrigPlan']].sample(frac=1).reset_index(drop=True)
+    df[['subNum', 'cueID', 'stimFinger', 'execMaxTime', 'feedbackTime', 'trialLabel', 'TrigExec',
+                          'TrigPlan', 'stimTrigExec', 'stimTrigPlan']] = shuffled_group2
 
-    filename = f"{experiment}_{session}_{subj}_{block + 1:02}.tgt"
+    # Compute startTime
+    start_times = [10000]
+    for i in range(1, len(df)):
+        prev = df.loc[i-1]
+        prev_sum = prev['planTime'] + prev['execMaxTime'] + prev['feedbackTime'] + prev['iti']
+        start_times.append(start_times[-1] + prev_sum + 500)
+    df['startTime'] = start_times
 
-    tgt.to_csv('target/' + filename, index=False, sep='\t')
-
-    print(filename)
+    # Save result
+    df.to_csv(f"target/{experiment}_{sn}_{run+1:02}.tgt", sep="\t", index=False)
